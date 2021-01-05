8864ac159c72aa1dd6980e4b00b2caab4acd393b
M1-73 أمل <76258869+am971@users.noreply.github.com>
2021-01-05 22:45:25 +0300

    done

--- question1.js
+++ question1.js
@@ -1,1 +1,9 @@
 // قم بحل السؤال الأول هنا
+let pets =[`cat`,`dog`,`turtle`,`bird`,`rabbit`];
+pets.pop();
+pets.push ("hamster");
+pets[2] = `more cats`;
+console.log (pets);
+let cats ={ name:`cat`,age:`2`,breeds:`Siamese`};
+cats[`emoje`]= `🐈` ;
+console.log (cats.age);
